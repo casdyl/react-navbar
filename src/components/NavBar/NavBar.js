df4a4f6f266ca0c93a05a2484f6b4055d1b1a7f3
Cassidy Lowe <cassidymlowe@gmail.com>
2022-08-14 16:57:45 -0700

    first commit react nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
+import "./NavBar.css";
+
+class NavBar extends Component {
+  state = { clicked: false };
+
+  render() {
+    return (
+      <nav className="NavBarItems">
+        <h1 className="navbar-logo">
+          React<i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.className} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
