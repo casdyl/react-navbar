4643b98ed5b8e6944888f295fa428572fdeebb4f
Cassidy Lowe <cassidymlowe@gmail.com>
2022-08-14 17:47:59 -0700

    complete nav bar tutorial

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
+import { Button } from "../Button";
 import "./NavBar.css";
 
 class NavBar extends Component {
   state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
   render() {
     return (
@@ -16,17 +21,18 @@ class NavBar extends Component {
             className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
           ></i>
         </div>
-        <ul>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
-                <a className={item.className} href={item.url}>
+                <a className={item.cName} href={item.url}>
                   {item.title}
                 </a>
               </li>
             );
           })}
         </ul>
+        <Button>Sign Up</Button>
       </nav>
     );
   }
